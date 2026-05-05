b0eb2e1117514b221eb4e9a26eaf1d32778f349e
Melvin Strobl <lc3267@kit.edu>
2026-05-05 11:48:14 +0200

    note
    Signed-off-by: Melvin Strobl <lc3267@kit.edu>

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-#TODO: add some basic argparse to generate the data
-#TODO: add some basic argparse to generate all figures
+#TODO: add some basic argparse to generate the data ("helmholtz.py", "poisson.py" and "var_poisson.py")
+#TODO: add some basic argparse to generate all figures ("plots.py" scripts)
